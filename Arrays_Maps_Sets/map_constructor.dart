326b88964b9b291676108cfad9145830d054e63f
Sherman Chen <uxdailygeek@gmail.com>
2020-01-29 15:19:23 +0800

    Added some Maps related examples

---
+++ Arrays_Maps_Sets/map_constructor.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  var myProducts = Map();
+  myProducts['first'] = 'TV';
+  myProducts['second'] = 'Mobile';
+  myProducts['third'] = 'Refrigerator';
+
+  if (myProducts.containsValue('Mobile')) {
+    print("Our products list has ${myProducts['second']}");
+  }
+}
