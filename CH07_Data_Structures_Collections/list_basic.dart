fd82567c277ce5684af7d3af631018584a7b7870
Sherman Chen <uxdailygeek@gmail.com>
2020-06-07 02:15:52 +0800

    Added 4 code examples for Chapter 7

---
+++ CH07_Data_Structures_Collections/list_basic.dart
@@ -0,0 +1,26 @@
+int listFunction() {
+  List<int> nameOfTest = List(3);
+  nameOfTest[0] = 1;
+  nameOfTest[1] = 2;
+  nameOfTest[2] = 3;
+
+  // there are three methods to capture the list
+  // 1. method
+  for (int element in nameOfTest) {
+    print(element);
+  }
+  print("------------");
+
+  // 2. method
+  nameOfTest.forEach((v) => print('${v}'));
+  print("------------");
+
+  // 3. method
+  for (int i = 0; i < nameOfTest.length; i++) {
+    print(nameOfTest[i]);
+  }
+}
+
+main(List<String> arguments) {
+  listFunction();
+}
